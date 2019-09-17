8d11a3db37c1566f33130ad2e793234d70f83fe9
Jeremie Potaznik <poirecraftStudio@gmail.com>
2019-09-17 14:49:39 +0200

    new laravel + dbs

---
+++ resources/views/login_Register.php
@@ -0,0 +1,37 @@
+<<!DOCTYPE html>
+<html>
+	<head>
+	<title>Login</title>
+	</head>
+	
+
+	<body>
+		<div class="LoginOrRegister"> 	
+			<h1> Welcome </h1>
+			<div class="Login">  
+				<!--      -->
+				<h2>Login</h2>
+				<span class="LoginLog"> 
+					<!--      -->
+					<h3> UserName : </h3>
+				</span>
+			</div> 	
+			<div class="Register"> 
+				<!--      -->
+				<h2>Register</h2>
+				<span class="RegisterLogin">
+					<!--      -->
+					<h3>UserName : </h3>
+				</span>
+				<span class="RegisterName>"> 
+					<!--      -->
+					<h3> </h3>
+				</span>
+				<>
+			</div>
+		</div>
+
+
+	</body>
+
+</html>
