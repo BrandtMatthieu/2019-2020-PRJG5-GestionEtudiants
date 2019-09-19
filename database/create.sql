ee4fbdae885db105204dfc4e884e07d1c3d30213
Jérémie Potaznik <48442@etu.he2b.be>
2019-09-19 11:47:40 +0200

    Fichier SQL completé

--- database/create.sql
+++ database/create.sql
@@ -16,3 +16,26 @@ CREATE TABLE "Subscriptions" (
 	"idCourse"	INTEGER NOT NULL,
 	PRIMARY KEY("idStudent","idCourse")
 )
+
+INSERT INTO Students VALUES (67554,"Phillipe","Arbre");
+INSERT INTO Students VALUES (59865,"Grégory","Lustucru");
+INSERT INTO Students VALUES (71009,"Thierry","Brocante");
+INSERT INTO Students VALUES (65834,"Edouard","LeBriscard");
+INSERT INTO Students VALUES (70128,"Jean","Poire");
+INSERT INTO Students VALUES (58702,"Bruce","Horloge");
+INSERT INTO Students VALUES (61039,"Peter","File");
+INSERT INTO Students VALUES (59286,"Bernard","Urluberlu");
+
+INSERT INTO Courses VALUES ("YMCA","Yoga Malin Comme Alain");
+INSERT INTO Courses VALUES ("SPQR","Senatus Populusque Romanus");
+INSERT INTO Courses VALUES ("JTM","Joute Terminale Magnifique");
+INSERT INTO Courses VALUES ("XD","Xénologie Délibérée");
+INSERT INTO Courses VALUES ("JPP","Jazz Précédant Poutine");
+
+INSERT INTO Subscriptions VALUES (67554,"YMCA");
+INSERT INTO Subscriptions VALUES (67554,"JPP");
+INSERT INTO Subscriptions VALUES (59865,"JPP");
+INSERT INTO Subscriptions VALUES (61039,"SPQR");
+INSERT INTO Subscriptions VALUES (70128,"JTM");
+INSERT INTO Subscriptions VALUES (65834,"XD");
+
