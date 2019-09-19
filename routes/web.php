de3b0ed0c3a2ae15802d474b6b675bdcc48806ea
Matthieu Brandt <44422@etu.he2b.be>
2019-09-19 09:37:07 +0200

    added routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,23 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/studentsList', function () {
+    return view('studentsList');
+});
+
+Route::get('/coursesList', function () {
+    return view('coursesList');
+});
+
+Route::get('/registerStudent', function () {
+    return view('registerStudent');
+});
+
+Route::get('/signupToCourse', function () {
+    return view('signupToCourse');
+});
+
+?>
