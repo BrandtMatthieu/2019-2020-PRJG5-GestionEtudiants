ce64a94b96e87aa0e5381825d1786fc6a39095d2
smrman <vinoa.89.jeux@gmail.com>
2019-10-03 11:17:55 +0200

    updated to Laravel 6.0.*

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,10 +2,18 @@
 
 namespace Tests\Unit;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class DatabaseTest extends TestCase
+class ExampleTest extends TestCase
 {
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
 }
